f334030b8157a12162a666136699e78b25dfb283
Ervin Pepic <pepic_ervin@hotmail.com>
2022-03-07 19:23:48 +0100

    refactor

--- day_24/mail_merger/main.py
+++ day_24/mail_merger/main.py
@@ -3,10 +3,6 @@
 # Replace the [name] placeholder with the actual name.
 # Save the letters in the folder "ReadyToSend".
 
-# Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
-# Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
-# Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
-
 
 with open("Input/Names/invited_names.txt") as contacts_list:
     friends_list = contacts_list.readlines()
