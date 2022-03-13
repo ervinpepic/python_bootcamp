145afe31bc82193eb0d214298737a089e56dce3d
Ervin Pepic <pepic_ervin@hotmail.com>
2022-03-13 23:39:54 +0100

    refactoring day 29

--- day_29/main.py
+++ day_29/main.py
@@ -1,5 +1,6 @@
 from tkinter import *
-from tkinter import messagebox
+import json
+from tkinter import messagebox, simpledialog
 from random import randint, choice, shuffle
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
@@ -34,19 +35,49 @@ def save_password():
     website_get = website_input.get()
     username_get = email_input.get()
     password_get = password_input.get()
+    new_data = {
+        website_get: {
+            "email": username_get,
+            "password": password_get
+        }
+    }
 
-    if (website_get != "" and username_get != "" and password_get != ""):
-        is_okey = messagebox.askokcancel(
-            "Save this data?", "Are you sure you want to save this data?")
-        if is_okey:
-            with open("passwords.txt", 'a') as save_password_to_text:
-                save_password_to_text.write(
-                    f"Website: {website_get} | Username: {username_get} | Password: {password_get}\n")
+    if (len(website_get) > 0 and len(password_get) > 0):
+        try:
+            with open("passwords.json", 'r') as json_file:
+                data = json.load(json_file)
+        except FileNotFoundError:
+            with open("passwords.json", 'w') as json_file:
+                json.dump(new_data, json_file, indent=4)
+        else:
+            data.update(new_data)
+
+            with open("passwords.json", 'w') as json_file:
+                json.dump(data, json_file, indent=4)
+        finally:
             website_input.delete(0, END)
             password_input.delete(0, END)
     else:
         messagebox.showinfo(
             title="Warning", message="Please populate all of these fields.")
+
+
+# ---------------------------- SEARCH ------------------------------- #
+
+def search_websites():
+    searched_phrase = website_input.get().capitalize()
+    if len(searched_phrase) > 0:
+        try:
+            with open("passwords.json", 'r') as json_file:
+                data_to_search = json.load(json_file)
+        except FileNotFoundError:
+            messagebox.showinfo(title=":(", message=f"Not found password DB")
+        else:
+            if searched_phrase in data_to_search:
+                email = data_to_search[searched_phrase]["email"]
+                password = data_to_search[searched_phrase]["password"]
+                messagebox.showinfo(title=f"{searched_phrase}",
+                                message=f"Email: {email}\nPassword: {password}")
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -75,6 +106,10 @@ password_label = Label(text="Password:")
 password_label.grid(row=5, column=2)
 password_input = Entry(width=21)
 password_input.grid(row=5, column=3)
+
+generate_button = Button(text="Search",
+                         width=10, command=search_websites)
+generate_button.grid(row=3, column=4)
 
 generate_button = Button(text="Generate Password",
                          width=10, command=generate_password)
