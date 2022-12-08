34e021ed98cd1f824afb02bf6162e6bc492b3b7a
Ervin Pepic <pepic_ervin@hotmail.com>
2022-12-08 16:23:04 +0100

    Added edit and delete methods and routes

--- day_67/blog/main.py
+++ day_67/blog/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
@@ -8,7 +8,7 @@ from flask_ckeditor import CKEditor, CKEditorField
 import datetime
 
 
-## Delete this code:
+# Delete this code:
 # import requests
 # posts = requests.get("https://api.npoint.io/43644ec4f0013682fc0d").json()
 
@@ -17,7 +17,7 @@ app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
-##CONNECT TO DB
+# CONNECT TO DB
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
@@ -25,7 +25,9 @@ db = SQLAlchemy(app)
 time = datetime.datetime.now()
 formated_time = time.strftime("%B %d, %Y")
 
-##CONFIGURE TABLE
+# CONFIGURE TABLE
+
+
 class BlogPost(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(250), unique=True, nullable=False)
@@ -36,7 +38,7 @@ class BlogPost(db.Model):
     img_url = db.Column(db.String(250), nullable=False)
 
 
-##WTForm
+# WTForm
 class CreatePostForm(FlaskForm):
     title = StringField("Blog Post Title", validators=[DataRequired()])
     subtitle = StringField("Subtitle", validators=[DataRequired()])
@@ -47,15 +49,72 @@ class CreatePostForm(FlaskForm):
 
 
 @app.route('/')
-def get_all_posts():
+def list_posts():
     posts = BlogPost.query.all()
-    return render_template("index.html", all_posts=posts)
+    return render_template("index.html", posts=posts)
 
 
 @app.route("/post/<int:post_id>")
 def show_post(post_id):
     requested_post = BlogPost.query.get(post_id)
     return render_template("post.html", post=requested_post)
+
+
+@app.route("/create-post", methods=["GET", "POST"])
+def create_post():
+    form = CreatePostForm()
+
+    if form.validate_on_submit():
+        add_post = BlogPost(
+            title=form.title.data,
+            date=formated_time,
+            body=form.body.data,
+            author=form.author.data,
+            img_url=form.img_url.data,
+            subtitle=form.subtitle.data
+        )
+
+        db.session.add(add_post)
+        db.session.commit()
+
+        return redirect(url_for("get_all_posts"))
+
+    return render_template("make-post.html", form=form)
+
+
+@app.route("/edit-post/<int:post_id>", methods=["GET", "POST"])
+def edit_post(post_id):
+    post = BlogPost.query.get(post_id)
+
+    edit_form = CreatePostForm(
+        title=post.title,
+        subtitle=post.subtitle,
+        img_url=post.img_url,
+        author=post.author,
+        body=post.body
+    )
+
+    if edit_form.validate_on_submit():
+        post.title = edit_form.title.data
+        post.date = formated_time
+        post.body = edit_form.body.data
+        post.author = edit_form.author.data
+        post.img_url = edit_form.img_url.data
+        post.subtitle = edit_form.subtitle.data
+
+        db.session.commit()
+
+        return redirect(url_for("show_post", post_id=post_id))
+
+    return render_template("make-post.html", post=post, form=edit_form)
+
+
+@app.route("/delete-post/<int:post_id>")
+def delete_post(post_id):
+    post = BlogPost.query.get(post_id)
+    db.session.delete(post)
+    db.session.commit()
+    return redirect(url_for("list_posts"))
 
 
 @app.route("/about")
@@ -66,24 +125,6 @@ def about():
 @app.route("/contact")
 def contact():
     return render_template("contact.html")
-
-
-@app.route("/new-post", methods=["GET", "POST"])
-def new_post():
-    form = CreatePostForm()
-    if form.validate_on_submit():
-        add_post = BlogPost(
-            title=form.title.data,
-            date=formated_time,
-            body=form.body.data,
-            author=form.author.data,
-            img_url=form.img_url.data,
-            subtitle=form.subtitle.data
-            )
-        db.session.add(add_post)
-        db.session.commit()
-        return redirect(url_for("get_all_posts"))
-    return render_template("make-post.html", form=form)
 
 
 if __name__ == "__main__":
